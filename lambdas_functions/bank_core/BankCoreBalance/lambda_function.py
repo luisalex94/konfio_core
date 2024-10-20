5621e3ff7795c3b6e5640e3b4c6ee804560baf76
AlejandroJR <luisalex944@gmail.com>
2024-10-20 08:24:27 -0600

    fix bug in table name

--- lambdas_functions/bank_core/BankCoreBalance/lambda_function.py
+++ lambdas_functions/bank_core/BankCoreBalance/lambda_function.py
@@ -1,7 +1,7 @@
 from repository import DynamoDBRepository
 from utils import get_body, calculate_balance, response_200, response_400, response_500
 
-database = DynamoDBRepository('bank_core_ddbb-core')
+database = DynamoDBRepository('bank_core_ddbb')
 
 def lambda_handler(event, context):
 
